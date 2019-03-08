4b11b40758889a39db76fc31154370fb161efe26
MaxouDriver <pocard.maxime@gmail.com>
2019-03-08 19:27:51 +0100

    Creation du projet VueJs et incorporation du template Vuetify Parallax

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app2">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app2',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
