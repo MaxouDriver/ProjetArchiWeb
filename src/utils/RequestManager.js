1f77717a1ef046e74441b18def0c6926088e8de5
MaxouDriver <pocard.maxime@gmail.com>
2019-03-10 10:31:19 +0100

    Ajout d'un outils permettant de faire des requetes

---
+++ src/utils/RequestManager.js
@@ -0,0 +1,61 @@
+const RequestManager = {
+    performGet(url, queries, successCallback, failureCallback){
+        var query = "";
+
+        if (queries != undefined) {
+            for (var k in queries){
+                if (query.length == 0) query += "?";
+                query += k + "=" + queries[k];
+            }
+        }
+        
+        var doGetAjax = async (url, query) => {
+            const response = await fetch(queries != undefined ? url + query : url, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            const body = await response.json();
+        
+            if (response.status === 401) {
+                //Not authenticated
+            }
+        
+            return body;
+        };
+    
+        doGetAjax(url, query).then(res => {
+            successCallback(res)
+        }).catch(err => {
+            failureCallback(err)}
+        );
+    },    
+    performPost(url, params, successCallback, failureCallback){
+        var doPostAjax = async (url, params) => {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(params)
+            });
+            const body = await response.json();
+        
+            if (response.status !== 200) {
+                //Not authenticated
+            }
+        
+            return body;
+        };
+    
+        doPostAjax(url, params).then(res => {
+            successCallback(res)
+        }).catch(err => {
+            failureCallback(err)}
+        );
+    }
+};
+
+export default RequestManager;
