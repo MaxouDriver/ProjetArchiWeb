7f680cbad31c1bfdbf269fc73ea6d95761999379
MaxouDriver <pocard.maxime@gmail.com>
2019-03-14 13:24:42 +0100

    Ajout de la base de données

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,14 @@
+var url = require('../serverInfos.json').nodeJServer
+
 module.exports = {
   devServer: {
       proxy: {
           '/api': {
-            target: 'http://localhost:4500',
+            target: url,
             ws: true,
             changeOrigin: true
           }
         },
     port: 3000
   }
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const firebase = require("firebase-admin");
 
 const app = express();
 const router = express.Router();
 const port = 4000;
+const serviceAccount = require("./serviceAccountKey.json");
+const databaseInformations = require("../serverInfos.json");
 
 app.use(bodyParser.urlencoded({ extended: true }, { limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
- 
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: databaseInformations.databaseUrl
+});
 
 zonesTouristiques = require('./routes/zonesTouristiques.js');
 
 router.route('/zonesTouristiques').get(zonesTouristiques.getZones);
-
-
 
 router.get('/', function(req, res) {
   res.json({ message: 'Nothing here!'});
