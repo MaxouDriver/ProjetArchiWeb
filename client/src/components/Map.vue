a543a7ec5854308206d895cd63ba4b45b447812f
MaxouDriver <pocard.maxime@gmail.com>
2019-03-22 21:02:52 +0100

    Ajout de l'API toilettes, avec l'integration des clusters leaflet

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -4,6 +4,7 @@
 <script>
 import DataManager from '../utils/DataManager';
 import L from 'leaflet';
+import 'leaflet.markercluster';
 
 export default {
   name: 'Map',
@@ -19,7 +20,7 @@ export default {
   mounted() {
     this.initMap();
     this.initLayers();
-    this.initZonesTouristiques();
+    this.initToilets();
   },
   methods: {
     initMap() {
@@ -66,6 +67,28 @@ export default {
       },function(err){
           thisRef.err = err
       });
+    },
+    initToilets(){
+      var thisRef = this;
+
+      DataManager.getToilets(function(res){
+        var markers = L.markerClusterGroup();
+
+        res.forEach(element => {
+
+            var title = element.fields.nom_voie;
+            var marker = L.marker(new L.LatLng(element.geometry.coordinates[1], element.geometry.coordinates[0]), { title: title});
+            marker.bindPopup(title);
+            markers.addLayer(marker);
+
+        });
+
+        thisRef.map.addLayer(markers);
+        //markers.addTo(thisRef.map);
+          
+      },function(err){
+          alert(err);
+      });
     }
   }
 }
@@ -78,4 +101,40 @@ export default {
   #leafletMap { 
     height: 100%;
   }
+
+  .info {
+    padding: 6px 8px;
+    font: 14px/16px Arial, Helvetica, sans-serif;
+    background: white;
+    background: rgba(255, 255, 255, 0.8);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+}
+.info h4 {
+    margin: 0 0 5px;
+    color: #777;
+}
+.legend {
+    text-align: left;
+    line-height: 18px;
+    color: #555;
+}
+.legend i {
+    width: 18px;
+    height: 18px;
+    float: left;
+    margin-right: 8px;
+    opacity: 0.7;
+}
+.legend .colorcircle {
+    border-radius: 50%;
+    width: 15px;
+    height: 15px;
+    margin-top: 0px;
+}
+.legend .circlepadding {
+    border-radius: 50%;
+    margin-top: 0px;
+    background: rgba(255, 255, 255, 0.8);
+}
 </style>
