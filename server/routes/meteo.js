02ed7ea2fce53de0ee8a75fec8eddcf9869a8e06
Fzwks <benjamin.nouveliere@outlook.fr>
2019-03-22 15:05:34 +0100

    API meteo et toilettes

--- server/routes/meteo.js
+++ server/routes/meteo.js
@@ -2,14 +2,14 @@ const RequestManager = require('../utils/RequestManager.js')
 const CacheManager = require('../utils/CacheManager.js');
 
 function getAllData(successCallback, failureCallback){
-	CacheManager.retreiveData("toilets",
+	CacheManager.retreiveData("weather",
 		function onDataRetrieved(storedData){
 			successCallback(storedData);
 		},
 		function onNoDataRetrieved(){
-			RequestManager.performGet("https://opendata.paris.fr/api/records/1.0/search/?dataset=sanisettesparis",
+			RequestManager.performGet("https://www.prevision-meteo.ch/services/json/lat=46.259lng=5.235",
 				function(receivedData){
-					CacheManager.storeData("toilets", receivedData,
+					CacheManager.storeData("weather", receivedData,
 						function(data){
 							successCallback(data);
 						},
@@ -28,11 +28,23 @@ function getAllData(successCallback, failureCallback){
 	);
 };
 
-exports.getToilets = function(req, res){
-    console.log("/getToilets called");
+exports.getWeather = function(req, res){
+    console.log("/getWeather called");
 	getAllData(
 		function(data){
-			res.json({success: true, data: data.records})
+			res.json({success: true, data: data})
+		},
+		function(err){
+			res.json({success: false, message: err});
+		}
+	);
+};
+
+exports.getWeatherCurrent = function(req, res){
+    console.log("/getWeatherOfToday called");
+	getAllData(
+		function(data){
+			res.json({success: true, data: {time: data.current_condition.hour, condition: data.current_condition.condition_key}})
 		},
 		function(err){
 			res.json({success: false, message: err});
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const router = express.Router();
 const port = 4000;
 const serviceAccount = require("./serviceAccountKey.json");
-const databaseInformations = require("../serverInfos.json"); 
+const databaseInformations = require("../serverInfos.json");
 
 app.use(bodyParser.urlencoded({ extended: true }, { limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -18,8 +18,15 @@ firebase.initializeApp({
 });
 
 zonesTouristiques = require('./routes/zonesTouristiques.js');
+toilets = require('./routes/toilettes.js');
+weather = require('./routes/meteo.js');
 
 router.route('/zonesTouristiques').get(zonesTouristiques.getZones);
+
+router.route('/toilets').get(toilets.getToilets);
+
+router.route('/weather').get(weather.getWeather);
+router.route('/weather/current').get(weather.getWeatherCurrent)
 
 router.get('/', function(req, res) {
   res.json({ message: 'Nothing here!'});
