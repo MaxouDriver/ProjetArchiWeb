4d12c9cf2654935079afce18487e4d124e569468
MaxouDriver <pocard.maxime@gmail.com>
2019-03-26 20:27:53 +0000

    Affichage du planning seulement si l'utilisateur est connecté

--- client/src/pages/Calendar.vue
+++ client/src/pages/Calendar.vue
@@ -1,29 +1,87 @@
 <template>
     <section>
-        <v-layout wrap>
-            <v-flex xs12 class="mb-12">
-            <v-sheet id="calendar" height="500">
-                <v-calendar ref="calendar" v-model="start" type="4day" :end="end" color="primary"></v-calendar>
-            </v-sheet>
-            </v-flex>
-        </v-layout>
+      <v-layout>
+        <v-flex v-if="authenticated">
+          <v-sheet height="400">
+            <!-- now is normally calculated by itself, but to keep the calendar in this date range to view events -->
+            <v-calendar ref="calendar" :now="today" :value="today" color="primary" type="week">
+              <!-- the events at the top (all-day) -->
+              <template v-slot:dayHeadere="{ date }">
+                <template v-for="event in eventsMap[date]">
+                  <!-- all day events don't have time -->
+                  <div v-if="!event.time" :key="event.title" class="my-event" @click="open(event)" v-html="event.title"></div>
+                </template>
+              </template>
+              <!-- the events at the bottom (timed) -->
+              <template v-slot:dayBody="{ date, timeToY, minutesToPixels }">
+                <template v-for="event in eventsMap[date]">
+                  <!-- timed events -->
+                  <div v-if="event.time" :key="event.title" :style="{ top: timeToY(event.time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
+                    class="my-event with-time" @click="open(event)" v-html="event.title"></div>
+                </template>
+              </template>
+            </v-calendar>
+          </v-sheet>
+        </v-flex>
+
+        <v-flex v-if="!authenticated">
+          <h1>You need to be authenticated to get a planning</h1>
+        </v-flex>
+      </v-layout>
     </section>
 </template>
 
 <script>
+import AuthenticationManager from '../utils/AuthenticationManager.js'
+
   export default {
     data: () => ({
-      type: 'month',
-      start: '2019-01-01',
-      end: '2019-01-06',
-      typeOptions: [
-        { text: 'Day', value: 'day' },
-        { text: '4 Day', value: '4day' },
-        { text: 'Week', value: 'week' },
-        { text: 'Month', value: 'month' },
-        { text: 'Custom Daily', value: 'custom-daily' },
-        { text: 'Custom Weekly', value: 'custom-weekly' }
+      isAuthenticated: false, 
+      today: '2019-03-27',
+      events: [
+        {
+          title: 'Weekly Meeting',
+          date: '2019-03-27',
+          time: '09:00',
+          duration: 45
+        },
+        {
+          title: 'Thomas\' Birthday',
+          date: '2019-03-28'
+        },
+        {
+          title: 'Mash Potatoes',
+          date: '2019-03-29',
+          time: '12:30',
+          duration: 180
+        }
       ]
-    })
+    }),
+    mounted() {
+      var thisRef = this;
+      this.isAuthenticated = AuthenticationManager.isAuthenticated();
+      this.$root.$on('authenticated', () => {
+        thisRef.isAuthenticated = true;
+      })
+      this.$root.$on('notauthenticated', () => {
+        thisRef.isAuthenticated = false;
+      })
+
+      this.$refs.calendar.scrollToTime('08:00')
+    },
+    computed: {
+      // convert the list of events into a map of lists keyed by date
+      eventsMap () {
+        const map = {}
+        this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e))
+        return map
+      }
+    },
+    methods: {
+      open (event) {
+        alert(event.title)
+      }
+    }
   }
+
 </script>
