d5bb535cf9ae766b80736f2a946292579658ee37
Fzwks <benjamin.nouveliere@outlook.fr>
2019-03-27 15:10:08 +0100

    Calendrier MaJ du style

--- client/src/pages/Calendar.vue
+++ client/src/pages/Calendar.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-      <v-layout>
+      <v-layout id="container">
         <v-flex v-if="isAuthenticated">
           <v-sheet height="400">
             <!-- now is normally calculated by itself, but to keep the calendar in this date range to view events -->
@@ -36,7 +36,7 @@ import AuthenticationManager from '../utils/AuthenticationManager.js'
 
   export default {
     data: () => ({
-      isAuthenticated: false, 
+      isAuthenticated: false,
       today: '2019-03-27',
       events: [
         {
@@ -85,3 +85,32 @@ import AuthenticationManager from '../utils/AuthenticationManager.js'
   }
 
 </script>
+
+<style scoped>
+  #container{
+    padding: 4vw;
+    margin: 4vw;
+  }
+  .my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #fff;
+  border: 1px solid #1867c0;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+  left: 4px;
+  margin-right: 8px;
+  position: relative;
+}
+.my-event.with-time {
+  position: absolute;
+  right: 4px;
+  margin-right: 0px;
+}
+
+</style>
