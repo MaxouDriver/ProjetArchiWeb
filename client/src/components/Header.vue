a3b9f0911ace6a0e7f423a38a64e2e1c1f47208f
MaxouDriver <pocard.maxime@gmail.com>
2019-03-27 13:54:18 +0000

    Ajout de la fonction connection au boutton connection

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,7 +3,7 @@
     <v-layout column align-center justify-center class="white--text">
       <h1 class="white--text mb-2 display-1 text-xs-center">{{title}}</h1>
       <div class="subheading mb-3 text-xs-center">{{subtitle}}</div>
-      <v-btn v-if="!isAuthenticated" class="blue lighten-2 mt-5" dark large  href="/pre-made-themes">
+      <v-btn v-if="!isAuthenticated" class="blue lighten-2 mt-5" dark large v-on:click="connect()">
         {{buttonName}}
       </v-btn>
     </v-layout>
@@ -22,6 +22,11 @@ export default {
           subtitle: "The most efficient way to discover Paris",
           buttonName: "Connection"
       }
+  },
+  methods: {
+    connect () {
+      this.$router.push("/login");
+    }
   },
   mounted() {
     var thisRef = this;
