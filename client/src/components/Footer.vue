f4808c00fdbc67a9a1c18cf8168aa5148c355746
MaxouDriver <pocard.maxime@gmail.com>
2019-04-12 08:31:41 +0000

    Mise a jour du footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -7,8 +7,9 @@
                 <div class="white--text ml-3">
                     Made with
                     <v-icon class="red--text">favorite</v-icon>
-                    by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-                    and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+                    by Benjamin Nouvelière, Renaud Loup et Maxime Pocard
+                    with <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+                    and with the help of <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
                 </div>
             </v-flex>
         </v-layout>
